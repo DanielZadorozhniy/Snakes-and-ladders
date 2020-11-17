88da37f1ffd9ac69e84e1324cbfeed0668b4495b
Danylo Zadorozhnyi <danylo_zadorozhnyi@epam.com>
2020-11-17 13:43:47 +0200

    Player tests

---
+++ src/test/java/com/game/snakes/player/PlayerTest.java
@@ -0,0 +1,20 @@
+package com.game.snakes.player;
+
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void initialPositionIsZero() {
+        Player player = new Player();
+        assertEquals(0, player.getPosition());
+    }
+
+    @Test
+    public void addToPositionIncreasesPreviousValue() {
+        Player player = new Player();
+        player.addToPosition(5);
+        player.addToPosition(5);
+        assertEquals(10, player.getPosition());
+    }
+}
