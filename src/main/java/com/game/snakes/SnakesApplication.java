c550201ec03bef892347cc816b260220f3c2fae0
Danylo Zadorozhnyi <danylo_zadorozhnyi@epam.com>
2020-11-17 13:29:43 +0200

    initial commit

---
+++ src/main/java/com/game/snakes/SnakesApplication.java
@@ -0,0 +1,13 @@
+package com.game.snakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnakesApplication.class, args);
+    }
+
+}
